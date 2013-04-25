16910f0a7e2ed261c3bd75eb354291ca3d35286d
Gregoire Heitz <gregoire.heitz@gmail.com>
2013-04-25 19:13:01 +0200

    removed useless stuff...

--- utils/intermediate.py
+++
@@ -1,3 +0,0 @@
-heightJoin = 0.0227536  
-angle = 0.87934595 
-Path="C:\Users\lis\Documents\FreeCAD\TestBatch\STL_Files\ParametricJoinPartA1.stl" 
--- utils/intermediatebis.py
+++
@@ -1,2 +0,0 @@
-angle = -0.10105443 
-Path="C:\Users\lis\Documents\FreeCAD\TestBatch\STL_Files\ParametricJoinPartB1.stl" 
