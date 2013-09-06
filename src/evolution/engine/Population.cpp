597e6aeb8d2715b6dc0722266553bbcfb7979b53
Titus Cieslewski <dev@titus-c.ch>
2013-09-06 14:33:37 +0200

    Load balancing in parallelization: Boost thread implementation of evaluation.

--- src/evolution/engine/Population.cpp
+++ src/evolution/engine/Population.cpp
@@ -28,12 +28,15 @@
 
 #include "evolution/engine/Population.h"
 #include <algorithm>
+#include <queue>
 #include <math.h>
 #include <boost/accumulators/accumulators.hpp>
 #include <boost/accumulators/statistics/stats.hpp>
 #include <boost/accumulators/statistics/mean.hpp>
 #include <boost/accumulators/statistics/max.hpp>
 #include <boost/accumulators/statistics/variance.hpp>
+#include <boost/thread.hpp>
+#include <boost/thread/mutex.hpp>
 #include "robogen.pb.h"
 #include "utils/network/ProtobufPacket.h"
 
@@ -78,9 +81,88 @@ boost::shared_ptr<RobotRepresentation> Population::getRobot(int n){
 	return robots_[n].robot;
 }
 
-// TODO use threads & load balancing to speed things up even further
+/**
+ * Thread function assigned to a socket
+ * @param indiQueue queue of Individuals to be evaluated
+ * @param queueMutex mutex for access to queue
+ * @param socket socket to simulator
+ * @param confFile simulator configuration file to be used for evaluations
+ */
+void evaluationThread(std::queue<Individual*> *indiQueue,
+		boost::mutex *queueMutex, TcpSocket *socket, std::string &confFile){
+	while (true){
+		boost::mutex::scoped_lock lock(*queueMutex);
+		if (indiQueue->empty()) return;
+		Individual *current = indiQueue->front(); indiQueue->pop();
+		std::cout << "." <<	std::flush;
+		lock.unlock();
+
+		// 1. Prepare message to simulator
+		boost::shared_ptr<robogenMessage::Robot> rsp =
+				boost::shared_ptr<robogenMessage::Robot>(
+						new robogenMessage::Robot(
+								current->robot->serialize()));
+		rsp->set_configuration(confFile);
+		ProtobufPacket<robogenMessage::Robot>	robotPacket(rsp);
+		std::vector<unsigned char> forgedMessagePacket;
+		robotPacket.forge(forgedMessagePacket);
+
+		// 2. send message to simulator
+		socket->write(forgedMessagePacket);
+
+		// 3. receive message from simulator
+		ProtobufPacket<robogenMessage::EvaluationResult> resultPacket(
+				boost::shared_ptr<robogenMessage::EvaluationResult>(
+						new robogenMessage::EvaluationResult()) );
+		std::vector<unsigned char> responseMessage;
+		socket->read(responseMessage,
+				ProtobufPacket<robogenMessage::EvaluationResult>::HEADER_SIZE);
+		// Decode the Header and read the payload-message-size
+		size_t msgLen = resultPacket.decodeHeader(responseMessage);
+		responseMessage.clear();
+		// Read the fitness payload message
+		socket->read(responseMessage, msgLen);
+		// Decode the packet
+		resultPacket.decodePayload(responseMessage);
+
+		// 4. write fitness to individual TODO exception
+		if(!resultPacket.getMessage()->has_fitness()) {
+			std::cerr << "Fitness field not set by Simulator!!!" <<
+					std::endl;
+			exit(EXIT_FAILURE);
+		}
+		else {
+			current->fitness = resultPacket.getMessage()->fitness();
+			current->evaluated = true;
+		}
+	}
+}
+
 void Population::evaluate(std::string confFile,
 		std::vector<TcpSocket*> &sockets){
+
+	// 1. Create mutexed queue of Individual pointers
+	std::queue<Individual*> indiQueue;
+	boost::mutex queueMutex;
+	for (unsigned int i=0; i<robots_.size(); i++){
+		indiQueue.push(&robots_[i]);
+	}
+
+	// 2. Prepare thread structure
+	boost::thread_group evaluators;
+
+	// 3. Launch threads
+	for (unsigned int i=0; i<sockets.size(); i++){
+		evaluators.add_thread(new boost::thread(evaluationThread, &indiQueue,
+				&queueMutex,sockets[i],confFile));
+	}
+
+	// 4. Join threads. Individuals are now evaluated.
+	evaluators.join_all();
+	// newline after per-individual dots
+	std::cout << std::endl;
+
+	/*
 	for (unsigned int i=0; i<robots_.size(); i+=sockets.size()){
 		for (unsigned int j=0; j<sockets.size(); j++){
 			boost::shared_ptr<robogenMessage::Robot> rsp =
@@ -129,6 +211,8 @@ void Population::evaluate(std::string confFile,
 			}
 		}
 	}
+	*/
+
 	// sort individuals by fitness, descending
 	std::sort(robots_.begin(), robots_.end(), operator >);
 
