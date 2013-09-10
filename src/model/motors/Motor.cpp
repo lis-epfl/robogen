4b0c5fc5fd00194fe3858003decd86bb4aafcfe9
Titus Cieslewski <dev@titus-c.ch>
2013-09-10 15:26:23 +0200

    Forgot this file in prev. commit

---
+++ src/model/motors/Motor.cpp
@@ -0,0 +1,41 @@
+/*
+ * @(#) Motor.cpp   1.0   Sep 10, 2013
+ *
+ * Titus Cieslewski (dev@titus-c.ch)
+ *
+ * The ROBOGEN Framework
+ * Copyright © 2013-2014 Titus Cieslewski
+ *
+ * Laboratory of Intelligent Systems, EPFL
+ *
+ * This file is part of the ROBOGEN Framework.
+ *
+ * The ROBOGEN Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (GPL)
+ * as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @(#) $Id$
+ */
+
+#include "model/motors/Motor.h"
+
+namespace robogen{
+
+Motor::Motor(NeuralNetworkRepresentation::ioPair id) : id_(id){
+
+}
+
+NeuralNetworkRepresentation::ioPair Motor::getId(){
+	return id_;
+}
+
+}
