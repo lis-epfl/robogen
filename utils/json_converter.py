3098a414b693761cfc1ccd17a6db3914e9d03f40
Josh Auerbach <joshua.auerbach@epfl.ch>
2014-05-01 10:12:11 +0200

    cleaned up a bit, plus made robust to handling weird robots, e.g. those without motors

--- utils/json_converter.py
+++ utils/json_converter.py
@@ -2,8 +2,8 @@ import json
 import sys
 
 
-def get_root(robot) :
-    for part in robot["body"]["part"] :
+def get_root(body) :
+    for part in body["part"] :
         if part["root"] :
             return part    
 
@@ -17,7 +17,7 @@ def fix_param(val) :
 
 
 def write_part(output, part, body, indentation_level=0) :
-    for i in range(indentation_level) :
+    for _ in range(indentation_level) :
         output.write("\t")
     if part["root"] :
         output.write("0")
@@ -36,45 +36,55 @@ def write_part(output, part, body, indentation_level=0) :
             output.write(" ")
             output.write(str(fix_param(param["paramValue"])))
     output.write("\n")
-    for connection in body["connection"] :
-        if connection["src"] == part["id"] :            
-            write_part(output, get_part(connection["dest"], body), body, indentation_level+1)
+    if "connection" in body :
+        for connection in body["connection"] :
+            if connection["src"] == part["id"] :            
+                write_part(output, get_part(connection["dest"], body), 
+                           body, indentation_level+1)
+
+def write_body(output, body):
+    write_part(output, get_root(body), body)
         
-
-
-if len(sys.argv) < 3 :
-    print "Usage: python json_converter.py input.json output.txt"
-    exit()
-
-robot = json.load(open(sys.argv[1],"r"))
-
-output = open(sys.argv[2], "w")
-
-root = get_root(robot)
-write_part(output, root, robot["body"])
-output.write("\n") 
-
-
-for connection in robot["brain"]["connection"] :
-    #print connection["weight"]
-    output.write(connection["src"].split("-")[0])
-    output.write(" ")
-    output.write(connection["src"].split("-")[1])
-    output.write(" ")
-    output.write(connection["dest"].split("-")[0])
-    output.write(" ")
-    output.write(connection["dest"].split("-")[1])
-    output.write(" ")
-    output.write(str(connection["weight"]))
+def write_brain(output, brain):
+    if "connection" in brain: #i no motors then no connections
+        for connection in brain["connection"] :
+            #print connection["weight"]
+            output.write(connection["src"].split("-")[0])
+            output.write(" ")
+            output.write(connection["src"].split("-")[1])
+            output.write(" ")
+            output.write(connection["dest"].split("-")[0])
+            output.write(" ")
+            output.write(connection["dest"].split("-")[1])
+            output.write(" ")
+            output.write(str(connection["weight"]))
+            output.write("\n")
     output.write("\n")
-output.write("\n")
-for neuron in robot["brain"]["neuron"] :
-    if neuron["layer"] != "input" :
-        output.write(neuron["id"].split("-")[0])
-        output.write(" ")
-        output.write(neuron["id"].split("-")[1])
-        output.write(" ")
-        output.write(str(neuron["biasWeight"]))
-        output.write("\n")
+    
+    if "neuron" in brain :
+        for neuron in brain["neuron"] :
+            if neuron["layer"] != "input" :
+                output.write(neuron["id"].split("-")[0])
+                output.write(" ")
+                output.write(neuron["id"].split("-")[1])
+                output.write(" ")
+                output.write(str(neuron["biasWeight"]))
+                output.write("\n")
+        
+if __name__ == "__main__":
+
+    if len(sys.argv) < 3 :
+        print "Usage: python json_converter.py input.json output.txt"
+        exit()
+    
+    robot = json.load(open(sys.argv[1],"r"))
+    
+    output = open(sys.argv[2], "w")
+    write_body(output, robot["body"])    
+    output.write("\n") 
+    if "brain" in robot :
+        write_brain(output, robot["brain"])
+    
+    
 
 
