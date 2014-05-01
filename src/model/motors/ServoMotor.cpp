5a7c5db520ba6045514f6ef0e47cdaa97e57109f
Josh Auerbach <joshua.auerbach@epfl.ch>
2014-05-01 19:58:50 +0200

    rollback velocity change while tested

--- src/model/motors/ServoMotor.cpp
+++ src/model/motors/ServoMotor.cpp
@@ -37,10 +37,8 @@ const float ServoMotor::DEFAULT_GAIN = 5.0;//0.5;
 const float ServoMotor::DEFAULT_MAX_FORCE = 25 * 9.81 * 1000;
 const float ServoMotor::MIN_POS_RAD = -(M_PI / 2) * 6 / 9;
 const float ServoMotor::MAX_POS_RAD = (M_PI / 2) * 6 / 9;
-
-// 100 rpm converted to rad/s
-const float ServoMotor::MIN_VELOCITY = -(100.0/60.0) * 2 * M_PI;
-const float ServoMotor::MAX_VELOCITY = (100.0/60.0) * 2 * M_PI;
+const float ServoMotor::MIN_VELOCITY = -3;
+const float ServoMotor::MAX_VELOCITY = 3;
 
 ServoMotor::ServoMotor(dJointID joint, float maxForce, float gain,
 		ioPair id) : Motor(id),
